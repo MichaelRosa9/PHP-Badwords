ebc9bbdaf391f297c5abe2ce4ff774d050d7ce0e
MichaelRosa9 <michaelrosa9@gmail.com>
2021-06-03 16:33:44 +0200

    test

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bad words</title>
+</head>
+<body>
+  <?php
+    $name = $_GET['name'];
+    $string1 = "Lorem";
+    $string2 = "ipsum dolor sit amet consectetur adipisicing elit. Libero alias vel, fugit nesciunt accusamus accusantium. Numquam facere vel voluptate. Ex dolore quo necessitatibus saepe minima quasi aliquid eum incidunt libero.";
+
+    ?>
+    <p> <?php echo string2 ?></p>
+
+</body>
+</html>
