078c7c86f560745ded0df1cd99473c286147f3cc
MichaelRosa9 <michaelrosa9@gmail.com>
2021-06-03 16:55:11 +0200

    get

--- index.php
+++ index.php
@@ -12,8 +12,14 @@
     $string1 = "Lorem";
     $string2 = "ipsum dolor sit amet consectetur adipisicing elit. Libero alias vel, fugit nesciunt accusamus accusantium. Numquam facere vel voluptate. Ex dolore quo necessitatibus saepe minima quasi aliquid eum incidunt libero.";
 
+    $string3 = "$string1 $string2";
+    $string4 = "$name $string2";
     ?>
-    <p> <?php echo string2 ?></p>
+    <p><strong>2 stringhe messe uno dopo l'altra:</strong> <?php echo $string1 ?> <?php echo $string2 ?></p>
+
+    <p><strong>Stringa concatenata:</strong> <?php echo $string3 ?></p>
+
+    <p><strong>Stringa concatenata con dentro '$_GET':</strong> <?php echo $string4 ?></p>
 
 </body>
 </html>
